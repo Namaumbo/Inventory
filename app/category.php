50b53e5f6a2122d3b62010265acc30bf7ff4ee2f
spikkernamaumbo <bsc-53-16@cc.ac.mw>
2021-02-06 20:33:29 +0200

    updated_database

--- app/category.php
+++ app/category.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class category extends Model
 {
+  protected $hidden=[
+      'category_id'
+  ];
+
      public function items()
      {
          return $this ->hasMany(items::class);
