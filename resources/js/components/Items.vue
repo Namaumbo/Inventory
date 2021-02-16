25cf4b90da7bef1856c35c61f77f5407137ecf12
spikkernamaumbo <bsc-53-16@cc.ac.mw>
2021-02-17 01:53:08 +0200

    Bootstrap Added

--- resources/js/components/Items.vue
+++ resources/js/components/Items.vue
@@ -1,20 +1,61 @@
 
 
 <template>
-<!--    <nav class="navbar navbar-default">-->
-<!--        <div class="container-fluid">-->
-<!--            <div class="navbar-header">-->
-<!--                <a class="navbar-brand" href="#">-->
-<!--                    <img alt="Brand" src="./assets/tree-logoa.jpg" height=70px >-->
-<!--                </a>-->
-<!--            </div>-->
+    <div class=only >
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">
+                    <img alt="Brand" src="./assets/tree-logoa.jpg" height=70px >
+                </a>
+            </div>
 
+        </div>
+        <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+            <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" color="white">
+             HOW IS</a>
+            <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-fill" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M8 3.293l6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6zm5-.793V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
+                <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
+            </svg> DASHBORD</a>
+            <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">
+
+                 BILLING</a>
+            <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings"
+               role="tab" aria-controls="v-pills-settings" aria-selected="false">
+                <i class="bi bi-alarm"></i> CLIENTS</a>
+        </div>
+        <div class="tab-content" id="v-pills-tabContent">
+            <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">...</div>
+            <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">...</div>
+            <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">...</div>
+            <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">...</div>
+        </div>
+
+    </nav>
+        <hr />
+<!--    <form>-->
+<!--        <div class="form-group">-->
+<!--            <label for="exampleInputEmail1">Email address</label>-->
+<!--            <input type="email" class="form-control" npm
+<!--        <div class="form-check">-->
+<!--            <input type="checkbox" class="form-check-input" id="exampleCheck1">-->
+<!--            <label class="form-check-label" for="exampleCheck1">Check me out</label>-->
 <!--        </div>-->
-<!--    </nav>-->
-    <button type="button" class="btn btn-primary">Primary</button>
+<!--        <button type="submit" class="btn btn-primary">Submit</button>-->
+<!--    </form>-->
+    </div>
  </template>
 <style>
+.only {
+    background-color: black;
+    width: 180px;
+    height:780px;
 
+}
+a{
+    color:white
+}
 </style>
 <script>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,10 @@
     </head>
     <body>
     <div id="app">
-        <Items></Items>
+        <div class="container-fluid">
+            <Items></Items>
+
+        </div>
 {{--        <Items></Items>--}}
     </div>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
