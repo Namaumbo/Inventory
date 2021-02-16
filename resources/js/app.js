cadd0672ecaae4d1f74d6d0ae217404a61472552
spikkernamaumbo <bsc-53-16@cc.ac.mw>
2021-02-16 02:37:55 +0200

    vuejsAdded

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
+import Vue from "vue";
+
 require('./bootstrap');
+window.Vue = require('vue');
+
+// window.VueRouter=require('vue-router').default;
+//
+// window.VueAxios=require('vue-axios').default;
+//
+// window.Axios=require('axios').default;
+
+ Vue.component('Items',require('./components/Items.vue').default);
+
+// Vue.use(VueRouter,VueAxios, axios);
+const app = new Vue({
+    el:'#app'
+})
